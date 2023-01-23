7570f3b7076d3d61ce4423b99ad4e2a9f27ac6b3
FueledByRage <erkmgwr@gmail.com>
2023-01-23 09:26:29 -0300

    App resources

--- src/main/java/com/microservice/imghandler/dtos/HandleImageDTO.java
+++ src/main/java/com/microservice/imghandler/dtos/HandleImageDTO.java
@@ -3,8 +3,8 @@ package com.microservice.imghandler.dtos;
 public class HandleImageDTO {
     public String width;
     public String height;
-    public String gravity;
     public String region;
     public String bucketName;
     public String fileName;
+    public String outpuKey;
 }
